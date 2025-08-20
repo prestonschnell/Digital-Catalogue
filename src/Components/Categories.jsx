1cbf17cfff7f093b6e28187ba21af9d681921599
Preston Schnell <64565042+prestonschnell@users.noreply.github.com>
2025-08-20 16:54:12 -0500

    Changed Stock Images.

--- src/Components/Categories.jsx
+++ src/Components/Categories.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './categoriesStyles.css'
 import Card from './Card.jsx'
+import Stock from '../assets/istockphoto-1147544807-612x612.jpg'
 
 const Categories = () => {
   return (
@@ -10,13 +11,10 @@ const Categories = () => {
               <h3>Categories</h3> 
             </div>
               <div className="categories-grid">
-                  <Card link="/category1" image="https://images.thdstatic.com/productImages/46ab3cfb-6093-435d-a6f0-c991006f05c3/svn/framing-studs-058449-64_100.jpg" title="All Inventory"/>
-                  <Card link="/product2" image="https://images.thdstatic.com/productImages/46ab3cfb-6093-435d-a6f0-c991006f05c3/svn/framing-studs-058449-64_100.jpg" title="Category 2"/>
-                  <Card link="/product3" image="https://images.thdstatic.com/productImages/46ab3cfb-6093-435d-a6f0-c991006f05c3/svn/framing-studs-058449-64_100.jpg" title="Category 3"/>
-                  <Card link="/product4" image="https://images.thdstatic.com/productImages/46ab3cfb-6093-435d-a6f0-c991006f05c3/svn/framing-studs-058449-64_100.jpg" title="Category 4"/>
-                  <Card link="/product5" image="https://images.thdstatic.com/productImages/46ab3cfb-6093-435d-a6f0-c991006f05c3/svn/framing-studs-058449-64_100.jpg" title="Category 5"/>
-                  <Card link="/product6" image="https://images.thdstatic.com/productImages/46ab3cfb-6093-435d-a6f0-c991006f05c3/svn/framing-studs-058449-64_100.jpg" title="Category 6"/>
-                  <Card link="/product7" image="https://images.thdstatic.com/productImages/46ab3cfb-6093-435d-a6f0-c991006f05c3/svn/framing-studs-058449-64_100.jpg" title="Category 7"/>
+                  <Card link="/category1" image={Stock} title="All Inventory"/>
+                  <Card link="/category2" image={Stock} title="Category 2"/>
+                  <Card link="/category3" image={Stock} title="Category 3"/>
+                  <Card link="/category4" image={Stock} title="Category 4"/>
               </div>  
         </div>
     </div>
--- src/Pages/ProductPage.jsx
+++ src/Pages/ProductPage.jsx
@@ -2,21 +2,40 @@ import React from 'react'
 import Nav from '../Components/Nav'
 import Footer from '../Components/Footer'
 import Stock from '../assets/istockphoto-1147544807-612x612.jpg'
+import '../Components/productPageStyles.css'
 
 const ProductPage = () => {
   return (
     <div>
         <Nav/>
-        <div classname="product-container">
-            <img src={Stock} alt="" />
-            <h1>Product Page</h1>
-            <h3>Here is the Name of the Product.</h3>
-            <p>here is a short description of each item for our product. </p>
-            <h2>Dimensions</h2>
-            <p># x #</p>
-            <p>Quantity: #</p>
-            Availability: # in Stock
+        
+        {/* Main Product Element */}
+        <div className="product-container">
+          <div className="img-product">
+              <img src={Stock} alt="" />
+          </div>
+          <div className="product-info">
+            <div className="product-name">
+              <h1>Here is the Name of the Product.</h1>
+              <p>here is a short description of each item for our product. </p>
             </div>
+            <div className="product-details">
+                <h2>Dimensions</h2>
+                <p># x #</p>
+                <p>Quantity: #</p>
+                <p>Availability: # in Stock</p>
+              </div>
+          </div>
+        </div>
+
+      {/* Dropdown Menu where we'll put the Product Details, Specifications, and Questions and Answers. */}
+        <div className="drop-menu">
+          
+        </div>
+
+
+
+        {/* Image Scroller with event set pieces being used. */}
       <Footer/>
     </div>
   )
